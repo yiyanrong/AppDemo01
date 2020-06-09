17b9f1d81c84f8d7b876735dfa8cbbadd82183dd
yiyanrong <17801099961@163.com>
2020-06-09 16:19:03 +0800

    Initial commit

---
+++ librays/src/main/java/com/example/librays/OneTest.java
@@ -0,0 +1,11 @@
+package com.example.librays;
+
+public class OneTest {
+    public static void main(String[] arg) {
+
+    }
+    public static  int sum(int a,int b){
+        return a+b;
+    }
+
+}
